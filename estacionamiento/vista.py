729bcbd405bd010da41465baa87317ebb84c2f53
ivanfortunesky <ibu_fortunesky@hotmail.com>
2024-05-16 11:24:58 -0300

    lamdas

--- estacionamiento/vista.py
+++ estacionamiento/vista.py
@@ -23,10 +23,10 @@ class Vista:
         Entry(root, textvariable=self.telefono).grid(row=4, column=1)
 
         # Botones
-        Button(root, text="Ingresar Auto", command=modelo.insertar_registro).grid(row=5, column=0)
-        Button(root, text="Consultar", command=modelo.consultar).grid(row=5, column=1)
-        Button(root, text="Borrar", command=modelo.borrar).grid(row=5, column=2)
-        Button(root, text="Modificar", command=modelo.modificar).grid(row=5, column=3)
+        Button(root, text="Ingresar Auto", command=lambda:self.insertar_registro).grid(row=5, column=0)
+        Button(root, text="Consultar", command=lambda:self.consultar).grid(row=5, column=1)
+        Button(root, text="Borrar", command=lambda:self.borrar).grid(row=5, column=2)
+        Button(root, text="Modificar", command=lambda:self.modificar).grid(row=5, column=3)
 
         # TREEVIEW
         self.tree = ttk.Treeview(root, columns=("Cochera", "Patente", "Nombre", "Teléfono"), show="headings")
@@ -38,22 +38,22 @@ class Vista:
 
                 ##################################################
         # Función para updatear las campos cuando selecciono un item
-        tree.bind("<<TreeviewSelect>>", actualizar)
-        
+        self.tree.bind("<<TreeviewSelect>>", actualizar)
+
         def actualizar(evento):
-            selection = tree.selection()
+            selection = self.tree.selection()
             if selection: 
-                cochera_seleccionada = tree.item(selection[0], "values")[0]  
-                cochera.set(cochera_seleccionada)  # hago  la cochera para ahorrarme pasos al modificar
+                cochera_seleccionada = self.tree.item(selection[0], "values")[0]  
+                self.cochera.set(cochera_seleccionada)  # hago  la cochera para ahorrarme pasos al modificar
             if selection: 
-                patente_seleccionada = tree.item(selection[0], "values")[1]  
-                patente.set(patente_seleccionada)  # hago  la patente para ahorrarme clicks al modificar
+                patente_seleccionada = self.tree.item(selection[0], "values")[1]  
+                self.patente.set(patente_seleccionada)  # hago  la patente para ahorrarme clicks al modificar
             if selection: 
-                nombre_seleccionada = tree.item(selection[0], "values")[2]  
-                nombre.set(nombre_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
+                nombre_seleccionada = self.tree.item(selection[0], "values")[2]  
+                self.nombre.set(nombre_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
             if selection: 
-                telefono_seleccionada = tree.item(selection[0], "values")[3]  
-                telefono.set(telefono_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
+                telefono_seleccionada = self.tree.item(selection[0], "values")[3]  
+                self.telefono.set(telefono_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
         
             
         ##################################################
