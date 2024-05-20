e5280482fdd530a81ad0b44e559befb8acbbc5e4
ivanfortunesky <ibu_fortunesky@hotmail.com>
2024-05-20 02:49:20 -0300

    cambio la instanciación de Vista y refresco comandos de VIM para insertar, guardar y salír

--- estacionamiento/controlador.py
+++ estacionamiento/controlador.py
@@ -2,6 +2,6 @@ from tkinter import Tk
 from vista import Vista
 
 if __name__ == "__main__":
-    root = Tk()
-    app = Vista(root)
-    root.mainloop()
+    raiz = Tk()
+    app = Vista(raiz)
+    raiz.mainloop()
