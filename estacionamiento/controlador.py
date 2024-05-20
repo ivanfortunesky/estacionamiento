6d1072439407b06ab8f7dceb807c0bae5f729a5f
ivanfortunesky <ibu_fortunesky@hotmail.com>
2024-05-20 13:45:30 -0300

    Migro a clase el controlador.py - Class Controlador

--- estacionamiento/controlador.py
+++ estacionamiento/controlador.py
@@ -1,7 +1,15 @@
 from tkinter import Tk
-from vista import Vista
+import vista
+
+class Controlador:
+
+    def __init__(self,raiz):
+        self.controlador_raiz = raiz
+        self.objeto_vista = vista.Vista(self.controlador_raiz)
+        
+
 
 if __name__ == "__main__":
     raiz = Tk()
-    app = Vista(raiz)
+    app = Controlador(raiz)
     raiz.mainloop()
