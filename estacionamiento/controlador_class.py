f6046a3cd78803ab59d9274ca364747607a0249a
ivanfortunesky <ibu_fortunesky@hotmail.com>
2024-06-02 20:05:25 -0300

    Unidad N°3 Programación y Excepciones

--- estacionamiento/controlador_class.py
+++ estacionamiento/controlador_class.py
@@ -5,12 +5,17 @@ import vista_class
 class Controlador:
 
     def __init__(self,raiz):
-        self.controlador_raiz = raiz
-        self.objeto_vista = vista_class.Vista(self.controlador_raiz)
-        
+        try:
+            self.controlador_raiz = raiz
+            self.objeto_vista = vista_class.Vista(self.controlador_raiz)
+        except Exception as error: 
+            print(f"except dentro de def __init__(self,raiz): y se produjo el error {error}") 
 
 
 if __name__ == "__main__":
-    raiz = Tk()
-    app = Controlador(raiz)
-    raiz.mainloop()
+    try:
+        raiz = Tk()
+        app = Controlador(raiz)
+        raiz.mainloop()
+    except Exception as error: 
+        print(f"except dentro de if __name__ == __main__: y se produjo el error {error}")
--- estacionamiento/vista_class.py
+++ estacionamiento/vista_class.py
@@ -7,62 +7,66 @@ from modelo_class import Modelo
 
 class Vista:
     def __init__(self, root):
-        self.root = root
-        self.modelo = Modelo()
-        self.root.title("Estacionamiento")
+        try:
+            self.root = root
+            self.modelo = Modelo()
+            self.root.title("Estacionamiento")
 
-        self.cochera = StringVar()
-        self.patente = StringVar()
-        self.nombre = StringVar()
-        self.telefono = StringVar()
+            self.cochera = StringVar()
+            self.patente = StringVar()
+            self.nombre = StringVar()
+            self.telefono = StringVar()
 
-        # Etiquetas y entradas
-        Label(root, text="Cochera").grid(row=1, column=0, sticky=W)
-        Entry(root, textvariable=self.cochera).grid(row=1, column=1)
-        Label(root, text="Patente").grid(row=2, column=0, sticky=W)
-        Entry(root, textvariable=self.patente).grid(row=2, column=1)
-        Label(root, text="Nombre y Apellido").grid(row=3, column=0, sticky=W)
-        Entry(root, textvariable=self.nombre).grid(row=3, column=1)
-        Label(root, text="Teléfono").grid(row=4, column=0, sticky=W)
-        Entry(root, textvariable=self.telefono).grid(row=4, column=1)
-
-
-        # Botones
-        Button(root, text="Ingresar Auto", command=lambda:self.modelo.insertar_registro(self.cochera,self.patente,self.nombre,self.telefono,self.tree)).grid(row=5, column=0)
-        Button(root, text="Consultar", command=lambda:self.modelo.consultar(self.tree)).grid(row=5, column=1)
-        Button(root, text="Borrar", command=lambda:self.modelo.borrar(self.tree)).grid(row=5, column=2)
-        Button(root, text="Modificar", command=lambda:self.modelo.modificar(self.cochera,self.patente,self.nombre,self.telefono,self.tree)).grid(row=5, column=3)
+            # Etiquetas y entradas
+            Label(root, text="Cochera").grid(row=1, column=0, sticky=W)
+            Entry(root, textvariable=self.cochera).grid(row=1, column=1)
+            Label(root, text="Patente").grid(row=2, column=0, sticky=W)
+            Entry(root, textvariable=self.patente).grid(row=2, column=1)
+            Label(root, text="Nombre y Apellido").grid(row=3, column=0, sticky=W)
+            Entry(root, textvariable=self.nombre).grid(row=3, column=1)
+            Label(root, text="Teléfono").grid(row=4, column=0, sticky=W)
+            Entry(root, textvariable=self.telefono).grid(row=4, column=1)
 
 
-        # TREEVIEW
-        self.tree = ttk.Treeview(root, columns=("Cochera", "Patente", "Nombre", "Teléfono"), show="headings")
-        self.tree.heading("Cochera", text="Cochera")
-        self.tree.heading("Patente", text="Patente")
-        self.tree.heading("Nombre", text="Nombre")
-        self.tree.heading("Teléfono", text="Teléfono")
-        self.tree.grid(row=7, column=0, columnspan=4)
+            # Botones
+            Button(root, text="Ingresar Auto", command=lambda:self.modelo.insertar_registro(self.cochera,self.patente,self.nombre,self.telefono,self.tree)).grid(row=5, column=0)
+            Button(root, text="Consultar", command=lambda:self.modelo.consultar(self.tree)).grid(row=5, column=1)
+            Button(root, text="Borrar", command=lambda:self.modelo.borrar(self.tree)).grid(row=5, column=2)
+            Button(root, text="Modificar", command=lambda:self.modelo.modificar(self.cochera,self.patente,self.nombre,self.telefono,self.tree)).grid(row=5, column=3)
 
 
-                ##################################################
-        # Función para updatear las campos cuando selecciono un item
-        self.tree.bind("<<TreeviewSelect>>", self.actualizar)   
+            # TREEVIEW
+            self.tree = ttk.Treeview(root, columns=("Cochera", "Patente", "Nombre", "Teléfono"), show="headings")
+            self.tree.heading("Cochera", text="Cochera")
+            self.tree.heading("Patente", text="Patente")
+            self.tree.heading("Nombre", text="Nombre")
+            self.tree.heading("Teléfono", text="Teléfono")
+            self.tree.grid(row=7, column=0, columnspan=4)
+
+
+                    ##################################################
+            # Función para updatear las campos cuando selecciono un item
+            self.tree.bind("<<TreeviewSelect>>", self.actualizar)
+        except Exception as error: 
+            print(f"except dentro de def __init__(self, root): y se produjo el error {error}")   
     def actualizar(self,evento):
-        selection = self.tree.selection()
-        if selection: 
-            cochera_seleccionada = self.tree.item(selection[0], "values")[0]  
-            self.cochera.set(cochera_seleccionada)  # hago  la cochera para ahorrarme pasos al modificar
-        if selection: 
-            patente_seleccionada = self.tree.item(selection[0], "values")[1]  
-            self.patente.set(patente_seleccionada)  # hago  la patente para ahorrarme clicks al modificar
-        if selection: 
-            nombre_seleccionada = self.tree.item(selection[0], "values")[2]  
-            self.nombre.set(nombre_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
-        if selection: 
-            telefono_seleccionada = self.tree.item(selection[0], "values")[3]  
-            self.telefono.set(telefono_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
+        try:
+            selection = self.tree.selection()
+            if selection: 
+                cochera_seleccionada = self.tree.item(selection[0], "values")[0]  
+                self.cochera.set(cochera_seleccionada)  # hago  la cochera para ahorrarme pasos al modificar
+            if selection: 
+                patente_seleccionada = self.tree.item(selection[0], "values")[1]  
+                self.patente.set(patente_seleccionada)  # hago  la patente para ahorrarme clicks al modificar
+            if selection: 
+                nombre_seleccionada = self.tree.item(selection[0], "values")[2]  
+                self.nombre.set(nombre_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
+            if selection: 
+                telefono_seleccionada = self.tree.item(selection[0], "values")[3]  
+                self.telefono.set(telefono_seleccionada)  # hago  el nombre para ahorrarme tipeo al modificar
         
-        #self.tree.bind("<<TreeviewSelect>>", self.actualizar)
-        ##################################################
+        except Exception as error: 
+            print(f"except dentro de actualizar(self,evento) y se produjo el error {error}")
 
 
         
